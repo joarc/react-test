838b7cd888128eccc511dab4bf4203905fb4bf70
Joar Classon <joarc@joarc.se>
2016-09-09 16:08:22 +0200

    home

--- dist/scripts/init.js
+++ dist/scripts/init.js
@@ -1,37 +1,55 @@
+var Index = React.createClass({
+  render: function() {
+    var title = "Funktionärssystem"
+    $("title").text(title);
+    return (
+      <Navbar title={title} />
+    );
+  }
+});
+
+var Navbar = React.createClass({
+  render: function() {
+    return (
+      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <header className="mdl-layout__header">
+          <div className="mdl-layout__header-row">
+            <span className="mdl-layout-title">{this.props.title}</span>
+            <div className="mdl-layout-spacer"></div>
+            <nav className="mdl-navigation mdl-layout--large-screen-only">
+              <a className="mdl-navigation__link" href="">Link</a>
+              <a className="mdl-navigation__link" href="">Link</a>
+              <a className="mdl-navigation__link" href="">Link</a>
+              <a className="mdl-navigation__link" href="">Link</a>
+            </nav>
+          </div>
+        </header>
+        <div className="mdl-layout__drawer">
+          <span className="mdl-layout-title">{this.props.title}</span>
+          <nav className="mdl-navigation">
+            <a className="mdl-navigation__link" href="">Link</a>
+            <a className="mdl-navigation__link" href="">Link</a>
+            <a className="mdl-navigation__link" href="">Link</a>
+            <a className="mdl-navigation__link" href="">Link</a>
+          </nav>
+        </div>
+        <main className="mdl-layout__content">
+          <div className="page-content">
+            <Body />
+          </div>
+        </main>
+      </div>
+    );
+  }
+});
+
+var Body = React.createClass({
+  render: function() {
+    return (<p>Test</p>);
+  }
+})
+
 ReactDOM.render(
   <Index />,
   document.getElementById('content')
 );
-ReactDOM.render(
-  <CommentBox />,
-  document.getElementById('test')
-);
-
-var CommentBox = React.createClass({
-  render: function() {
-    return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
-      </div>
-    );
-  }
-});
-
-var Index = React.createClass({
-  render: function() {
-    return (
-      /*<div className="jumbotron">
-        <h1>React Test</h1>
-        <p>Some description</p>
-        <p>
-          <a href="#" className="btn btn-lg btn-primary">
-            Btn
-          </a>
-        </p>
-      </div>*/
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
-      </div>
-    );
-  }
-});
